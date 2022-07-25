0a8604108f7365b786e3d53e0274a5fd95966dfd
riquesantt <henriquesv07@gmail.com>
2022-07-25 20:57:08 -0300

    feat: Adicionando ThemeContext

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom"
 
+import { useAppThemeContext } from "../shared/contexts/ThemeContext";
+
 
 export const AppRoutes = () => {
+    const { toggleTheme } = useAppThemeContext();
+
     return(
     <Routes> 
-        <Route path="*/pagina-inicial" element={<Button variant='contained' color='primary'> Página Inicial</Button> } /> 
+        <Route path="*/pagina-inicial" element={<Button variant='contained' color='primary' onClick={ toggleTheme}> Trocar Tema</Button> } /> 
 
-        <Route path="*" element={<Navigate to="*/pagina-inicial"/> }/>
+
+        <Route path="*" element={<Navigate to="*/pagina-inicial"/> }/>  
     </Routes>
     );
 }
--- src/shared/themes/Dark.ts
+++ src/shared/themes/Dark.ts
@@ -1,1 +1,25 @@
-export { };
+import { createTheme } from '@mui/material'
+import { yellow, cyan } from '@mui/material/colors';
+
+export const DarkTheme = createTheme({
+    palette: {
+     primary: {
+        main: yellow[600],
+        dark: yellow[800],
+        light: yellow[400],
+        contrastText:'white',
+    },
+
+    secondary: {
+        main: cyan[500],
+        dark: cyan[400],
+        light:cyan[300],
+        contrastText:'#fffff',
+    },
+
+    background: {
+        default: '#303134',
+        paper: '#202124',
+    }      
+}
+}); 
